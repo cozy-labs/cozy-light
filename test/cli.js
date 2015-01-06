1dd7edf77cae063ec3ace61ae2123a1f0b416e78
cnacnt <clement.auger@yrnet.com>
2015-01-06 16:38:09 +0800

    add CLI test skeleton

---
+++ test/cli.js
@@ -0,0 +1,15 @@
+
+var should = require('should');
+
+
+describe.skip('CLI', function () {
+  it('displays help', function(){});
+  it('starts properly', function(){});
+  it('can install plugin', function(){});
+  it('can install app', function(){});
+  it('can install distro', function(){});
+  it('displays content properly', function(){});
+  it('can remove app', function(){});
+  it('can remove plugin', function(){});
+  it('can stop properly', function(){});
+});
