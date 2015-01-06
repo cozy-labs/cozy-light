a81a906e7970c4cc10f4a9966bf70ad79eb148b8
clement <maboiteaspam@wanadoo.fr>
2015-01-07 00:20:15 +0800

    add mutexer capabilities add new status command

--- test/cli.js
+++ test/cli.js
@@ -5,6 +5,8 @@ require('should');
 describe.skip('CLI', function () {
   it('displays help', function(){});
   it('starts properly', function(){});
+  it('does not start twice', function(){});
+  it('creates the mutex', function(){});
   it('can install plugin', function(){});
   it('can install app', function(){});
   it('can install distro', function(){});
@@ -12,4 +14,5 @@ describe.skip('CLI', function () {
   it('can remove app', function(){});
   it('can remove plugin', function(){});
   it('can stop properly', function(){});
+  it('deletes the mutex', function(){});
 });
