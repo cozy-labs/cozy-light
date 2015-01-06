ca8be2eecba5c63941a1d5df8672ef782747d3f7
clement <maboiteaspam@wanadoo.fr>
2015-01-07 00:15:45 +0800

    improve mocha integration

--- run-tests.bat
+++ run-tests.bat
@@ -1,13 +1,13 @@
 
 :: about '|| exit /b' see http://stackoverflow.com/a/21912169
 
-mocha --no-exit --globals s,cooked --reporter spec test/actions.js || exit /b
-mocha --no-exit --globals s,cooked --reporter spec test/application-helpers.js || exit /b
-mocha --no-exit --globals s,cooked --reporter spec test/config-helpers.js || exit /b
-mocha --no-exit --globals s,cooked --reporter spec test/config-watcher.js || exit /b
-mocha --no-exit --globals s,cooked --reporter spec test/cozy-light.js || exit /b
-mocha --no-exit --globals s,cooked --reporter spec test/functional.js || exit /b
-mocha --no-exit --globals s,cooked --reporter spec test/main-app-helpers.js || exit /b
-mocha --no-exit --globals s,cooked --reporter spec test/node-helpers.js || exit /b
-mocha --no-exit --globals s,cooked --reporter spec test/npm-helpers.js || exit /b
-mocha --no-exit --globals s,cooked --reporter spec test/plugin-helpers.js || exit /b
+mocha --no-exit test/actions.js || exit /b
+mocha --no-exit test/application-helpers.js || exit /b
+mocha --no-exit test/config-helpers.js || exit /b
+mocha --no-exit test/config-watcher.js || exit /b
+mocha --no-exit test/cozy-light.js || exit /b
+mocha --no-exit test/functional.js || exit /b
+mocha --no-exit test/main-app-helpers.js || exit /b
+mocha --no-exit test/node-helpers.js || exit /b
+mocha --no-exit test/npm-helpers.js || exit /b
+mocha --no-exit test/plugin-helpers.js || exit /b
--- run-tests.sh
+++ run-tests.sh
@@ -1,13 +1,14 @@
+#!/bin/sh
 set -e
 set -x
 
-mocha --globals s,cooked --reporter spec test/actions.js
-mocha --globals s,cooked --reporter spec test/application-helpers.js
-mocha --globals s,cooked --reporter spec test/config-helpers.js
-mocha --globals s,cooked --reporter spec test/config-watcher.js
-mocha --globals s,cooked --reporter spec test/cozy-light.js
-mocha --globals s,cooked --reporter spec test/functional.js
-mocha --globals s,cooked --reporter spec test/main-app-helper.js
-mocha --globals s,cooked --reporter spec test/node-helpers.js
-mocha --globals s,cooked --reporter spec test/npm-helpers.js
-mocha --globals s,cooked --reporter spec test/plugin-helpers.js
+mocha test/actions.js
+mocha test/application-helpers.js
+mocha test/config-helpers.js
+mocha test/config-watcher.js
+mocha test/cozy-light.js
+mocha test/functional.js
+mocha test/main-app-helper.js
+mocha test/node-helpers.js
+mocha test/npm-helpers.js
+mocha test/plugin-helpers.js
