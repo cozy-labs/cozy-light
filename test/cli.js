8c5c6fff8d9c9303c002661d188f899b1d73e397
clement <maboiteaspam@wanadoo.fr>
2015-01-08 20:13:03 +0800

    indent

--- test/cli.js
+++ test/cli.js
@@ -5,43 +5,43 @@ require('should');
 
 
 describe('CLI', function () {
-    it('displays help', function(done){
-        var output = '';
-        spawn('cozy-light', ['--help'])
-            .on('data', function (d) {
-                output += d;
-            })
-            .on('close', function () {
-                console.error(output);
-                output.should.match(/some/);
-                done();
-            });
-    });
+  it('displays help', function(done){
+    var output = '';
+    spawn('cozy-light', ['--help'])
+      .on('data', function (d) {
+        output += d;
+      })
+      .on('close', function () {
+        console.error(output);
+        output.should.match(/some/);
+        done();
+      });
+  });
   it('starts properly', function(done){
-      var cozyProcess = spawn('cozy-light', ['--start'])
-          .on('close', function () {
-              done();
-          });
-      setTimeout(function(){
-          request.get('http://localhost:19104/' , function(error, response){
-              response.statusCode.match(/404/);
-              cozyProcess.kill();
-          });
-      },1500);
+    var cozyProcess = spawn('cozy-light', ['--start'])
+      .on('close', function () {
+        done();
+      });
+    setTimeout(function(){
+      request.get('http://localhost:19104/' , function(error, response){
+        response.statusCode.match(/404/);
+        cozyProcess.kill();
+      });
+    },1500);
   });
   it('does not start twice', function(){
-      var cozyProcess = spawn('cozy-light', ['--start'])
-          .on('close', function (code) {
-              code.should.eql(0);
-          });
-      setTimeout(function(){
-          spawn('cozy-light', ['--start'])
-              .on('close', function (code) {
-                  code.should.eql(1);
-                  cozyProcess.kill();
-                  done();
-              });
-      },1500);
+    var cozyProcess = spawn('cozy-light', ['--start'])
+      .on('close', function (code) {
+        code.should.eql(0);
+      });
+    setTimeout(function(){
+      spawn('cozy-light', ['--start'])
+        .on('close', function (code) {
+          code.should.eql(1);
+          cozyProcess.kill();
+          done();
+        });
+    },1500);
   });
   it.skip('creates the mutex', function(){});
   it.skip('can install plugin', function(){});
@@ -51,15 +51,15 @@ describe('CLI', function () {
   it.skip('can remove app', function(){});
   it.skip('can remove plugin', function(){});
   it('can stop properly', function(){
-      var cozyProcess = spawn('cozy-light', ['--start'])
-          .on('close', function (code) {
-              code.should.eql(0);
-              done();
-          });
-      setTimeout(function(){
-          request.get('http://localhost:19104/' , function(){
-              cozyProcess.kill();
-          });
-      },1500);});
+    var cozyProcess = spawn('cozy-light', ['--start'])
+      .on('close', function (code) {
+        code.should.eql(0);
+        done();
+      });
+    setTimeout(function(){
+      request.get('http://localhost:19104/' , function(){
+        cozyProcess.kill();
+      });
+    },1500);});
   it.skip('deletes the mutex', function(){});
 });
