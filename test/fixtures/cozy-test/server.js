b47ebd35c8d9c438298c60d1fc923f41be41ca9c
clement <maboiteaspam@wanadoo.fr>
2015-01-26 23:25:16 +0800

    pluginize pouchdb

--- test/fixtures/cozy-test/server.js
+++ test/fixtures/cozy-test/server.js
@@ -8,15 +8,13 @@ var application = module.exports.start = function(options, callback) {
   options.root = options.root || __dirname;
   options.port = options.port || process.env.PORT;
 
-  if (options.db !== null) {
-    var app = express();
-    app.get('/', function(req, res, next) {
-      res.status(200).send({ok: true});
-    });
-    var server = app.listen(options.port, function (err) {
-      callback(err, app, server);
-    });
-  }
+  var app = express();
+  app.get('/', function(req, res, next) {
+    res.status(200).send({ok: true});
+  });
+  var server = app.listen(options.port, function (err) {
+    callback(err, app, server);
+  });
 };
 
 if (!module.parent) {
